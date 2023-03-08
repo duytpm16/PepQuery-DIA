e5a5f0dd61be0f4966cbeef0879e353c952a1a68
duytpm16 <duyp@venus.zhang-lab.org>
2023-03-08 15:57:40 -0600

    Test 1

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include <fstream>
 #include <iostream>
 #include <math.h>
+#include <sstream>
 #include <string>
 #include <vector>
 
-int main() {
+int main(int argc, char* argv[]) {
 
     std::vector<Network*> net;
     for (int i = 0; i < 12; i++) {
@@ -15,7 +16,40 @@ int main() {
         net.push_back(nn);
     }
 
-    std::ifstream scorefile("scores1.out", std::ifstream::in);
-    
+    std::string file(argv[1]);
+    std::ifstream scorefile(file, std::ifstream::in);
+
+    std::string line;
+    std::vector<float> vscores;
+    float s;
+
+    std::ofstream results("predicted_target_scores.out", std::ios_base::app);
+    std::ostringstream oss;
+    oss << "Average\n";
+    while (std::getline(scorefile, line)) {
+        std::istringstream iss(line);
+        while(iss >> s) vscores.push_back(s);
+
+        float* scores = &vscores[0];
+        auto dataset = createDataSet(1, vscores.size(), &scores);
+
+        double avg = 0.0;
+        double n = 12.0;
+        for (int i = 0; i < 12; i++) {
+            forwardPassDataSet(net[i], dataset);
+            auto result = getOuput(net[i]);
+            avg += result->data[0];
+        }
+        avg /= n;
+        oss  << avg << "\n";
+
+        vscores.clear();
+    }
+
+    results << oss.str();
+    oss.str(std::string());
+    oss.clear();
+    results.close();
+
     return 0;
 }
